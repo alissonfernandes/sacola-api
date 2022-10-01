1bcce5f2f09ad7b3ff4d5c85d770578ba67cca5f
alissonfernandes <alissonfernandes200@gmail.com>
2022-10-01 19:27:24 -0300

    create DAO (Repository)

---
+++ src/main/java/br/com/sacola/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package br.com.sacola.repository;
+
+import br.com.sacola.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
