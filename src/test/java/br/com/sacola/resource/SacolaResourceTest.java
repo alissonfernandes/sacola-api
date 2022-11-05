83522e959ba394a6b0853ca49e7de9d4769b7a8c
alissonfernandes <alissonfernandes200@gmail.com>
2022-11-05 16:45:37 -0300

    test method POST 'incluirItemNaSacola' exception return

--- src/test/java/br/com/sacola/resource/SacolaResourceTest.java
+++ src/test/java/br/com/sacola/resource/SacolaResourceTest.java
@@ -1,8 +1,8 @@
 package br.com.sacola.resource;
 
 import br.com.sacola.enumeration.FormaPagamento;
-import br.com.sacola.exception.BagIsEmptyException;
-import br.com.sacola.exception.BagNotFoundException;
+import br.com.sacola.exception.*;
+import br.com.sacola.resource.dto.ItemDTO;
 import br.com.sacola.service.SacolaServiceImpl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,6 +23,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.not;
@@ -31,7 +32,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 @SpringBootTest
 @AutoConfigureMockMvc
-@DisplayName("test method HTTP response")
+@DisplayName("test sacola resource")
 public class SacolaResourceTest {
 
     @Autowired
@@ -83,5 +84,49 @@ public class SacolaResourceTest {
         when(sacolaService.fecharSacola(VALID_ID, 1)).thenThrow(BagIsEmptyException.class);
         mockMvc.perform(patch(SACOLA_API_URL_PATH + "/fecharSacola/" + VALID_ID +"?formaPagamento=1")
                 .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    @DisplayName("when method HTTP POST 'incluirItemNaSacola' is called with JSON 'sacolaId' invalid then return an exception")
+    void httpPostIncluirItemNaSacolaWithSacolaIdInvalid() throws Exception {
+        ItemDTO itemDTO = new ItemDTO(1L, 1, INVALID_ID);
+        when(sacolaService.incluirItemNaSacola(itemDTO)).thenThrow(BagNotFoundException.class);
+        mockMvc.perform(post(SACOLA_API_URL_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(itemDTO)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("when method HTTP POST 'incluirItemNaSacola' is called and JSON 'sacolaId' is closed then return an exception")
+    void httpPostIncluirItemNaSacolaWhenBagClosed() throws Exception {
+        ItemDTO itemDTO = new ItemDTO(1L, 1, VALID_ID);
+        when(sacolaService.incluirItemNaSacola(itemDTO)).thenThrow(BagIsClosedException.class);
+        mockMvc.perform(post(SACOLA_API_URL_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(itemDTO)))
+                .andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    @DisplayName("when HTTP POST 'incluirItemNaSacola' is called and product from JSON 'produtoId' not found then return an exception")
+    void httpPostIncluirItemNaSacolaProductNotFound() throws Exception {
+        ItemDTO itemDTO = new ItemDTO(0L, 1, VALID_ID);
+        when(sacolaService.incluirItemNaSacola(itemDTO)).thenThrow(ProductNotFoundException.class);
+        mockMvc.perform(post(SACOLA_API_URL_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(itemDTO)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("when HTTP POST 'incluirItemNaSacola' is called and item is different restaurant then return an exception")
+    void httpPostIncluirItemNaSacolaProducDifferent() throws Exception {
+        ItemDTO itemDTO = new ItemDTO(1L, 1, VALID_ID);
+        when(sacolaService.incluirItemNaSacola(itemDTO)).thenThrow(ProductDifferentException.class);
+        mockMvc.perform(post(SACOLA_API_URL_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(itemDTO)))
+                .andExpect(status().isNotAcceptable());
     }
 }
